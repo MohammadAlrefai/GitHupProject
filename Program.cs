2a1637dd6e9ed5f437ef9e86439b2b4135f8f953
MohammedAlrefai <malrefai30@outlook.com>
2020-10-01 09:42:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHupProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
